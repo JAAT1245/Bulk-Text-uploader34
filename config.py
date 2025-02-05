25be35022af86e20714035ef38749a78dc2f4c89
Rajesh R Mahar <136462044+rajrmahar@users.noreply.github.com>
2025-02-05 21:07:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("AUTH_USERS")
+    BOT_TOKEN = os.environ.get("")
+    API_ID = int(os.environ.get(""))
+    API_HASH = os.environ.get("")
+    AUTH_USERS = os.environ.get("")
